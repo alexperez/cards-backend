d1087705fef05ffd07837004d7e769aca381ea96
Alex Perez <alex.alb.perez@gmail.com>
2020-12-23 16:19:50 -0500

    Add cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const initDB = require("./database");
 const session = require("./config/session");
@@ -12,15 +13,14 @@ const session = require("./config/session");
 const app = express();
 
 initDB()
-    .then((dbConnection) => {
-        app.emit("ready", dbConnection);
-    })
+    .then((dbConnection) => app.emit("ready", dbConnection))
     .catch(console.error);
 
 app.on("ready", (dbConnection) => {
     app.set("trust proxy", 1);
     app.use(helmet());
     app.use(morgan("tiny"));
+    app.use(cors());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use(session(dbConnection));
