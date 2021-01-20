9af16c3a7baf3d7c42b9f43b92d4cc54871e200c
Alex Perez <alex.alb.perez@gmail.com>
2021-01-20 14:50:28 -0500

    Add sets controller and middleware

---
+++ controllers/sets.js
@@ -0,0 +1,106 @@
+const Set = require("../database/schemas/Set");
+
+const queryPublicOrUser = (user) => ({
+    $or: [{ public: true }, { user: user && user.id }],
+});
+
+exports.queryAll = (req, res, next) => {
+    const { user } = req.session;
+    res.locals.query = queryPublicOrUser(user);
+
+    next();
+};
+
+exports.queryTopic = (req, res, next) => {
+    const { user } = req.session;
+    const { topic } = req.params;
+
+    res.locals.query = {
+        $and: [{ topic }, queryPublicOrUser(user)],
+    };
+
+    next();
+};
+
+exports.list = async (req, res, next) => {
+    try {
+        const { query } = res.locals;
+        const sets = await Set.find(query)
+            .populate("user", "id, username")
+            .sort({ createdAt: -1 })
+            .limit(20)
+            .exec();
+
+        res.status(200).json({ sets });
+    } catch (e) {
+        next(e);
+    }
+};
+
+exports.create = async (req, res, next) => {
+    try {
+        const { title, public, topic, cards } = req.body;
+        const {
+            user: { id },
+        } = req.session;
+
+        const set = new Set({ title, public, user: id, cards, topic });
+        await set.save();
+
+        res.status(200).json({ message: "Set successfully created.", set });
+    } catch (e) {
+        next(e);
+    }
+};
+
+exports.load = async (req, res, next, id) => {
+    try {
+        const { user } = req.session;
+        const set = await Set.findOne({
+            $and: [{ _id: id }, queryPublicOrUser(user)],
+        }).populate("user", "id, username");
+
+        if (!set) {
+            return res.status(404).json({ message: "Set not found." })
+        }
+
+        res.locals.set = set;
+        next();
+    } catch (e) {
+        next(e);
+    }
+}
+
+exports.show  = (req, res, next) => {
+    const { set } = res.locals;
+    res.status(200).json({ set });
+};
+
+exports.update = async (req, res, next) => {
+    try {
+        const { set } = res.locals;
+        const { title, public, cards, topic } = req.body;
+        
+        set.title = title;
+        set.public = public;
+        set.cards = cards;
+        set.topic = topic;
+
+        await set.save();
+
+        res.status(200).json({ message: "Set updated successfully.", set });
+    } catch (e) {
+        next(e);
+    }
+};
+
+exports.delete = async (req, res, next) => {
+    try {
+        const { set } = res.locals;
+        await Set.deleteOne(set);
+
+        res.status(200).json({ message: "Set deleted successfully." })
+    } catch (e) {
+        next(e);
+    }
+};
--- middlewares/auth/index.js
+++ middlewares/auth/index.js
@@ -1,7 +1,7 @@
 const { authValidatorRules, authValidator } = require("./validator");
 const rateLimiter = require("./rate-limit");
 
-const isAuthenticated = (req, res, next) => {
+exports.isAuthenticated = (req, res, next) => {
     const { user } = req.session;
 
     if (!user) {
@@ -13,9 +13,15 @@ const isAuthenticated = (req, res, next) => {
     next();
 };
 
-module.exports = {
-    authValidatorRules,
-    authValidator,
-    rateLimiter,
-    isAuthenticated,
+exports.setsAuth = (req, res, next) => {
+    const { user } = req.session;
+    const { set } = res.locals;
+
+    if (set.user.id === user.id || user.admin) return next();
+
+    res.status(401).json({ message: "Unauthorized." });
 };
+
+exports.authValidatorRules = authValidatorRules;
+exports.authValidator = authValidator;
+exports.rateLimiter = rateLimiter;
